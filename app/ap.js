c2bb72f8027d209938940b1045057fa72979ea93
Christine <christinekhekama99@gmail.com>
2024-06-13 15:18:25 +0300

    array basic methods

---
+++ app/ap.js
@@ -0,0 +1,35 @@
+// // adds element at the end (.push)
+// const myName = ['chris', 'jay', 'khekame'];
+// myName.push('Mulwa')
+// console.log(myName);
+
+// //  adds an element at the begining (.unshift)
+// const myName = ['chris', 'jay', 'khekame'];
+// myName.unshift('Mulwa')
+// console.log(myName);
+
+
+// // removes last element (.pop)
+// const myName = ['chris', 'jay', 'khekame'];
+// myName.pop();
+// console.log(myName);
+
+// // removes first element (.shift)
+// const myName = ['chris', 'jay', 'khekame'];
+// myName.shift();
+// console.log(myName);
+
+// // returning the length of the arrays (.length)
+// const myName = ['chris', 'jay', 'khekame'];
+// console.log(myName.length);
+
+// // returning the index number of the array (.indexOf)
+// const myName = ['chris', 'jay', 'khekame'];
+// console.log(myName.indexOf('jay'));
+
+
+// // confirms if the element exist in the array. returns true or false (.includes)
+// const myName = ['chris', 'jay', 'khekame'];
+// console.log(myName.includes('Mulwa'));
+
+
