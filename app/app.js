63a9e2055b7bfa43c0f083a139627937575faa44
Christine <christinekhekama99@gmail.com>
2024-06-13 13:34:04 +0300

    array tips with tip value

--- app/app.js
+++ app/app.js
@@ -1,9 +1,13 @@
-
+// funtion input bills returning tips
 function calcTip(bill) {
     return (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.20;
 }
 
 console.log(calcTip(555)); 
-
+// array bills with test data
 const bills = [125, 555, 44];
+// array called tips containing tip value
+let tip1 = calcTip(bills[0]);
+let tip2 = calcTip(bills[1]);
+let tip3 = calcTip(bills[2]);
 
